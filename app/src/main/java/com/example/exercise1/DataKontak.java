470d30fae9cd31fc4e85a19bbf7e654c5cc0aa43
Rayka Agustasya <raykaagustasya16@gmail.com>
2021-04-05 22:38:29 +0700

    add data kontak

---
+++ app/src/main/java/com/example/exercise1/DataKontak.java
@@ -0,0 +1,15 @@
+package com.example.exercise1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DataKontak extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_kontak);
+
+    }
+}
