5c706c9c5c1368bcf082d53d86e415210458e2f0
Rayka Agustasya <raykaagustasya16@gmail.com>
2021-04-05 22:39:23 +0700

    add all

---
+++ app/src/main/java/com/example/exercise1/Pendaftaran.java
@@ -0,0 +1,15 @@
+package com.example.exercise1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pendaftaran extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pendaftaran);
+    }
+}
